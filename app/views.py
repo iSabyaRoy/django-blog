0741038db89002d3c2a0de7308ac20984300cd72
iSabyaRoy <112532847+iSabyaRoy@users.noreply.github.com>
2023-05-10 17:23:57 +0600

    add template

--- app/views.py
+++ app/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello this is my first django project")
+    return render(request, 'index.html')
